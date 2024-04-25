deeb791520a9126d84d68420651daa946195c4a9
Vaughan <jwst0208@gmail.com>
2024-04-25 15:56:36 +0900

    Create latest_posts.py

---
+++ latest_posts.py
@@ -0,0 +1,64 @@
+import feedparser
+
+blog_url = "https://webb-c.github.io/feed.xml"
+rss_feed = feedparser.parse(blog_url)
+
+MAX_NUM = 5
+
+latest_posts = ""
+
+for idx, entrie in enumerate(rss_feed['entries']):
+  if idx >= MAX_NUM:
+     break;
+  feed_date = entrie['published_parsed']
+  latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
+
+print(latest_posts)
+
+preREADME = """
+
+```python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class Researcher:
+
+    def __init__(self):
+        self.name = "Vaughan Sohn"
+        self.role = "Researcher"
+        self.major = "Computer Science and Engineering"
+        self.interest = {
+            research=["Quantum Computing", "Optimization Theory", "Reinforcement Learning"],
+            others=["Astronomy", "Quantum Physics"]
+        }
+
+    def callout(self):
+        print("우주의 아름다움도 다양한 지식을 접하며 스스로의 생각이 짜여나갈 때 불현듯 나를 덮쳐오리라.")
+
+
+me = Researcher()
+me.callout()
+```
+### Hi, there 👋
+- 🔭 I'm currently studying CSE in Konkuk University.
+- 💡 Tech Blog: https://webb-c.github.io/
+- 📮 How to reach me: jwst0208@gmail.com
+- 🚀 [Resume](https://webb-c.github.io/)
+
+#### Languages and Tools:
+![Python](https://img.shields.io/badge/-Python-black?logo=Python&style=social)&nbsp;&nbsp;
+![C](https://img.shields.io/badge/c-%2300599C.svg?style=social&logo=c)&nbsp;&nbsp;
+![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=social&logo=c%2B%2B)&nbsp;&nbsp;
+![PyTorch](https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=social&logo=PyTorch)&nbsp;&nbsp;
+![Qiskit](https://img.shields.io/badge/Qiskit-%236929C4.svg?style=social&logo=Qiskit)&nbsp;&nbsp;
+![LaTeX](https://img.shields.io/badge/latex-%23008080.svg?style=social&logo=latex)&nbsp;&nbsp;
+<a href="https://solved.ac/profile/jwst0210"><img src="http://mazassumnida.wtf/api/mini/generate_badge?boj=jwst0210"/></a>
+
+#### Latest Updated Post:
+"""
+
+resultREADME = f"{preREADME}{latest_posts}"
+
+with open("README.md", "w", encoding='utf-8') as f :
+  f.write(resultREADME)
